f643ebec61eddbe3264f0127135537a4924005b4
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 12:31:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/voiceassistant/main.go
@@ -0,0 +1,23 @@
+// cmd/voiceassistant/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "voiceassistant/internal/voiceassistant"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := voiceassistant.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
